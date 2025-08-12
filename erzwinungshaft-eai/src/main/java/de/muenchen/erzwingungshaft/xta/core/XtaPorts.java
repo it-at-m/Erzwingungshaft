c2bfe7d2f916ed691ddf34ad65cb9857091d5295
langehm <38280183+langehm@users.noreply.github.com>
2025-08-12 09:47:55 +0200

    added wrapper for all three ports used for communicating

---
+++ erzwinungshaft-eai/src/main/java/de/muenchen/erzwingungshaft/xta/core/XtaPorts.java
@@ -0,0 +1,24 @@
+package de.muenchen.erzwingungshaft.xta.core;
+
+import genv3.de.xoev.transport.xta.x211.ManagementPortType;
+import genv3.de.xoev.transport.xta.x211.MsgBoxPortType;
+import genv3.de.xoev.transport.xta.x211.SendPortType;
+
+public record XtaPorts (
+
+        /*
+         * used for: createMessageId, cancelMessage, getTransportReport, lookupService, checkAccountActive
+         */
+        ManagementPortType managementPortType,
+
+        /*
+         * used for: close, getNextStatusList, getNextMessage, getStatusList, getMessage
+         */
+        MsgBoxPortType msgBoxPortType,
+
+        /*
+         * used for: sendMessageSync, sendMessage
+         */
+        SendPortType sendPortType
+){
+}
