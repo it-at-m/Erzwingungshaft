6af5ae3eb132a6fb33ce50ff984fef3e8424fea0
langehm <38280183+langehm@users.noreply.github.com>
2025-09-04 11:11:58 +0200

    added portFactory as bean

--- erzwinungshaft-eai/src/main/java/de/muenchen/erzwingungshaft/xta/config/XtaServiceConfig.java
+++ erzwinungshaft-eai/src/main/java/de/muenchen/erzwingungshaft/xta/config/XtaServiceConfig.java
@@ -1,14 +1,28 @@
 package de.muenchen.erzwingungshaft.xta.config;
 
+import de.muenchen.erzwingungshaft.xta.core.XtaClientService;
+import de.muenchen.erzwingungshaft.xta.core.XtaServicePorts;
+import de.muenchen.erzwingungshaft.xta.core.XtaServicePortsFactory;
+import de.muenchen.erzwingungshaft.xta.exception.XtaClientInitializationException;
 import genv3.de.xoev.transport.xta.x211.XTAService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class XtaServiceConfig {
 
+    private final XtaServicePortsFactory xtaServicePortsFactory;
+
     @Bean
-    public XTAService xtaService(/* ggf. Abhängigkeiten hier injizieren */) {
+    public XTAService xtaService() {
         return new XTAService();
+    }
+
+    @Bean
+    public XtaServicePorts xtaServicePorts() throws XtaClientInitializationException {
+        return xtaServicePortsFactory.create();
     }
 }
